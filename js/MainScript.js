cc9512bd3fc2d0406b249feeba9ac46add87ace3
Kallin10 <hannah.tornatore@gmail.com>
2025-10-12 20:49:28 -0300

    Agg3 T3 Aberta

--- js/MainScript.js
+++ js/MainScript.js
@@ -113,9 +113,9 @@ function ButtonChangePage3(){
 }
 
 function ButtonChangePage4(){
-	//PageType = 4;
-	//PageChoose();
-	alert("Você aperta o botão da sala secreta, mas nada acontece além de uma risada sinistra ao fundo... Será que foi o Jerry?");
+	PageType = 4;
+	PageChoose();
+	//alert("Você aperta o botão da sala secreta, mas nada acontece além de uma risada sinistra ao fundo... Será que foi o Jerry?");
 }
 
 function ButtonChangePage5(){
@@ -270,5 +270,6 @@ function Prova01(){ //AGG3
 //	  alert("Acesso negado!");
 //	}
 //  }
+
 
 
